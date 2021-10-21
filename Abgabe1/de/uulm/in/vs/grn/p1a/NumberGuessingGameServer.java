7eb0a4dc591943fa8124001a0a47a92d3de9b9bd
Saman Karimi <saman@karimi.de>
2021-10-21 15:07:41 +0200

    finished

--- Abgabe1/de/uulm/in/vs/grn/p1a/NumberGuessingGameServer.java
+++ Abgabe1/de/uulm/in/vs/grn/p1a/NumberGuessingGameServer.java
@@ -1,6 +1,7 @@
 package de.uulm.in.vs.grn.p1a;
 
 import java.net.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.io.*;
 
 public class NumberGuessingGameServer{
@@ -11,5 +12,66 @@ public class NumberGuessingGameServer{
         System.out.println("server started");
         Socket server = serverSocket.accept();
 
+        OutputStream out = server.getOutputStream();
+        InputStream in = server.getInputStream();
+
+        int counter = 0;
+        int number = ThreadLocalRandom.current().nextInt(0,50);
+        int playerguess;
+
+        boolean game = true;
+        while (game) {
+
+            try {
+
+                int x;
+                String input = "";
+
+                while ((x = in.read()) != -1) {
+
+                    if (x == '\n') {
+
+                        out.write(input.getBytes());
+                        playerguess = Integer.parseInt(input);
+                        System.out.println(playerguess);
+
+                        if (counter == 6) {
+
+                            out.write("You Lost!".getBytes());
+                            game = false;
+                            break;
+
+                        } else if (playerguess == number) {
+
+                            out.write("Congratulation!".getBytes());
+                            game = false;
+                            break;
+
+                        } else if (playerguess < number) {
+
+                            out.write("that's too low!".getBytes());
+                            counter++;
+
+                        } else if (playerguess > number) {
+
+                            out.write("that's too high!".getBytes());
+                            counter++;
+                        }
+                        input = "";
+
+                    } else {
+
+                        System.out.println((char) x);
+                        input += (char) x;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            out.write("\nThank you".getBytes());
+        }
+        server.close();
+        serverSocket.close();
     }
 }
